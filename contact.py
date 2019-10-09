8df084a2b669a9c3d72271e2cdf6ec514cf791fd
Geksa <123@mail.ru>
2019-10-09 17:37:56 +0300

    1-е задание готово.Дополнительный класс

---
+++ contact.py
@@ -0,0 +1,15 @@
+
+class Contact:
+
+    def __init__(self, firstname, secondname, surname, nickname, title, company, address, home, mobile, work, email):
+        self.firstname=firstname
+        self.secondname=secondname
+        self.surname=surname
+        self.nickname=nickname
+        self.title=title
+        self.company=company
+        self.address=address
+        self.home=home
+        self.mobile=mobile
+        self.work=work
+        self.email=email
