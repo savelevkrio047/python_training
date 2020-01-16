6e8a024de2e4b73674f2c53ada89b873a3193dfb
Geksa <123@mail.ru>
2020-01-16 19:02:08 +0300

    3-й урок. 2-я часть. Оптимизация ввода данных в формы, отлаженное ifelse

--- fixture/group.py
+++ fixture/group.py
@@ -2,6 +2,24 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        #open modif form
+        wd.find_element_by_name("edit").click()
+        #fill form
+        self.fill_group_form(new_group_data)
+        #submit modification
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+
+
+
+
+
+
     def return_to_group_page(self):
         # Проверяем наличие созданной группы
         wd = self.app.wd
@@ -14,19 +32,26 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         wd.find_element_by_name("new").click()
-        # заполнение формы создания группы
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # Сохраняем новую форму
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        # заполнение формы создания группы
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
 
     def open_groups_page(self):
         # открываем страницу с группами
@@ -36,6 +61,10 @@ class GroupHelper:
         #Delete first user
         wd = self.app.wd
         self.open_groups_page()
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         wd.find_element_by_name("delete").click()
         self.app.open_home_page()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
