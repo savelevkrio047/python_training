4e0e26646c9d1bcedb27d4d33b7de2c36e165572
Geksa <123@mail.ru>
2020-02-02 22:36:22 +0300

    6-e занятие. Урок 8 Отделение тестовых данных от сценариев (DDT продолжается). Отлажено

--- test/test_add_group2.py
+++ test/test_add_group2.py
@@ -5,17 +5,8 @@ from fixture.session import SessionHelper
 from fixture.application import Application
 import random
 import string
-
-def random_string(prefix, maxlen):
-   symbols =  string.ascii_letters + string.digits + " "*10
-   return  prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [Group(name="", header="", footer="")] + [Group(name=random_string("name",10), header=random_string("header",20), footer=random_string("footer", 20))
-            for i in range(5)]
-testdata1 = [Group(name=name, header=header, footer=footer)
-            for name in ["", random_string("name", 10)]
-            for header in ["", random_string("header", 10)]
-            for footer in ["", random_string("footer", 10)]]
+from data.add_group import constant as testdata
+from data.add_group import testdata1
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
