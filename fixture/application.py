b74eda8ba2c3012ab0632114b02de639db87b205
Geksa <123@mail.ru>
2020-02-02 21:21:42 +0300

    5-e занятие.   Урок 8 Передача параметров в тесты из командной строки при запуске. Отлажено

--- fixture/application.py
+++ fixture/application.py
@@ -14,16 +14,24 @@ class Application:
         except:
             return False
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif  browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("unrecognized browser %S" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.user = UserHelper(self)
+        self.base_url = base_url
 
     def open_home_page(self):
         # открытие главной страницы
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         wd = self.wd
