e6d727108825b30012045e6189022afe5c7eb022
Geksa <123@mail.ru>
2020-02-05 18:18:01 +0300

    6-e занятие.  Урок 10 Еще 1 способ параметризации тестов_динамическая генерация тестов. Отлажено

--- test/test_add_group2.py
+++ test/test_add_group2.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.group import Group
-from fixture.session import SessionHelper
-from fixture.application import Application
-import random
-import string
-from data.add_group import constant as testdata
-from data.add_group import testdata1
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group2(app, group):
+def test_add_group2(app, data_groups):
+        group = data_groups
         old_groups = app.group.get_group_list()
        # group = Group(name="surmame", header="123123", footer="123123")
         app.group.create(group)
